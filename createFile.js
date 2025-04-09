fc2ef3d22d48f8b7dde0e014d98a6414751b21c4
Luiz Gomes <louisnuuk@hotmail.com>
2025-04-09 12:13:17 -0300

    feat

--- createFile.js
+++ createFile.js
@@ -12,29 +12,8 @@ const filePath = path.join(dirPath, 'index.ts');
 const content = `
 import { createServerNextArchitecture, createClientNextArchitecture, ApiEndpoint } from "@caucolum/api-query-object";
 
-export interface BreedsImageRandomDataProps {
-    message: string;
-    status: string;
-}
-
-export interface BreedHoundImagesDataProps {
-    message: string[];
-    status: string;
-}
-
 const api = {
-    breeds_image_random: {
-        url: "https://dog.ceo/api/breeds/image/random",
-        authenticated: false,
-        method: 'get',
-        DATA_PROPS: {} as BreedsImageRandomDataProps,
-    },
-    breed_hound_images: {
-        url: "https://dog.ceo/api/breed/hound/images",
-        authenticated: false,
-        method: 'get',
-        DATA_PROPS: {} as BreedHoundImagesDataProps,
-    },
+    
 } as const satisfies Record<string, ApiEndpoint>;
 
 const serverQueriesObject = createServerNextArchitecture(api);
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -7,11 +7,13 @@ type ApiConfig = {
         authenticated: boolean;
         ARGS_PROPS?: unknown;
         DATA_PROPS?: unknown;
+        ERROR_PROPS?: unknown;
     };
 };
-interface ApiClientResourcesProps<T = any, K = any> {
+interface ApiClientResourcesProps<T = any, K = any, M = any> {
     makeRequest: (props?: K) => void;
     status: UseServiceCallStatusProps;
+    error: M;
     data: T;
     args: K;
 }
@@ -19,7 +21,7 @@ type ServerApiMethods<T extends ApiConfig> = {
     [K in keyof T]: (params?: T[K]['ARGS_PROPS']) => Promise<T[K]['DATA_PROPS']>;
 };
 type ClientApiMethods<T extends ApiConfig> = {
-    [K in keyof T]: (params?: any) => ApiClientResourcesProps<T[K]["DATA_PROPS"], T[K]["ARGS_PROPS"]>;
+    [K in keyof T]: (params?: any) => ApiClientResourcesProps<T[K]["DATA_PROPS"], T[K]["ARGS_PROPS"], T[K]["ERROR_PROPS"]>;
 };
 
 interface ApiEndpoint<ArgsProps = unknown, DataProps = unknown> {
